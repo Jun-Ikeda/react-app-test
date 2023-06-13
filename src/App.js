805676e50f4e83257005d826d8dbcf33fccc105f
Jun Ikeda (HX80G) <jun.ikeda0121@gmail.com>
2023-06-13 22:01:14 +0000

    test app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>I'm wondering why I have to edit App.js instead of index.js.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
